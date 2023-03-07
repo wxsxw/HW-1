00f4fab37974264ab50c9b1fb0f02be06bcbd1cf
Svetlana Zakharchenko <wxsxw2011@gmail.com>
2023-03-07 23:09:03 +0200

    adativ

--- js/menu.js
+++ js/menu.js
@@ -31,3 +31,23 @@
     refs.body.classList.toggle("no-scroll");
   }
 })();
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelectorAll("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
+//   // for (let i = 0; i < refs.openModalBtn.length; i++) {
+//   //   refs.openModalBtn[i].addEventListener("click", toggleModal);
+//   // }
+//   refs.openModalBtn.forEach((elem) => {
+//     elem.addEventListener("click", toggleModal);
+//   });
+
+//   // refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
